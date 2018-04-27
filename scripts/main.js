61243b3a5074b3c16ceb6c86ed27e3f3b5de3c15
Greg Korte <gregmkorte@gmail.com>
2018-04-27 10:20:53 -0500

    Adds functionality for all buttons.

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,31 @@
-let matrix = randomize(50, 50);  //Randomizes initial state
+let matrix;
+let iteration;
 
 document.addEventListener('DOMContentLoaded', function(){  //Loads the DOM
 
 	function populate(){  //Places populate() into memory
 		let table = document.getElementById('grid');  //Creates variable for table with ID grid
+		console.log(table);
 		table.innerHTML = '';  //Clears table for population
 		for (i = 0; i < matrix.length; i++){  //Iterates through first tier array
 			let row = document.createElement('tr');  //Creates variable & tr element
 			table.appendChild(row);  //Appends tr to table
 			for (j = 0; j < matrix[i].length; j++){  //Iterates through second tier array
 				let data = document.createElement('td');  //Creates variable & td element
-
-				function existenceState(){  //Places existenceState into memory
-					if (matrix[i][j] === 1){  //Sets up conditions for classes
-						data.setAttribute('class', 'alive');  //Sets class to alive
-						}
-					else {  //Sets class to dead
-						data.setAttribute('class', 'dead');
-					}
-				}
 			 	row.appendChild(data);  //Appends tr with td
-			  	data.innerHTML = matrix[i][j];  //Places data into td
-			  	existenceState();  //Executes existenceState to apply classes
+		  	data.innerHTML = matrix[i][j];  //Places data into td
+		  	existenceState(matrix[i][j], data);  //Executes existenceState to apply classes
 			}
 		};
+	}
+
+	function existenceState(mat, data){  //Places existenceState into memory
+		if (mat === 1){  //Sets up conditions for classes
+			data.setAttribute('class', 'alive');  //Sets class to alive
+			}
+		else {  //Sets class to dead
+			data.setAttribute('class', 'dead');
+		}
 	}
 
 	function livingNeighborCount(x, y){  //Places livingNeighborCount() into memory
@@ -89,22 +91,28 @@ document.addEventListener('DOMContentLoaded', function(){  //Loads the DOM
 	}
 
 	document.querySelector('#populate').addEventListener('click', function(){
+		clearInterval(iteration);
+		matrix = randomize(50, 50);
 		populate();  //Executes populate() to create grid
-		document.querySelector('#tick').addEventListener('click', function(){  //Adds event listener for Tick button
-			matrix = populateNextState(matrix);
-			populate(matrix);
-		});
-		document.querySelector('#run').addEventListener('click', function(){  //Adds event listener for Run button
-			matrix = populateNextState(matrix);
-			setInterval(function(){
-				matrix = populateNextState(matrix);
-				populate()
-			}, 10);
-		});
-		document.querySelector('#stop').addEventListener('click', function(){  //Adds event listener for Stop button
-			matrix = populateNextState(matrix);
-		});
 	})
+
+	document.querySelector('#tick').addEventListener('click', function(){  //Adds event listener for Tick button
+		clearInterval(iteration);
+		matrix = populateNextState(matrix);
+		populate(matrix);
+	});
+
+	document.querySelector('#run').addEventListener('click', function(e){  //Adds event listener for Run button
+		matrix = populateNextState(matrix);
+		iteration = setInterval(function(){
+			matrix = populateNextState(matrix);
+			populate();
+		}, 10);
+	});
+
+	document.querySelector('#stop').addEventListener('click', function(e2){  //Adds event listener for Stop button
+		clearInterval(iteration);
+	});
 
 });
 
